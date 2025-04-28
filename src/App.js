b8ba90cbc51caad86e44a1f4ab1a902b3be5c0e7
Kira37 <kirubelenyew76@gmail.com>
2025-04-28 11:39:56 +0300

    hide the navbar whenever the user is on login or register page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
-import React from "react";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
+import React, { useEffect, useState } from "react";
+import {
+  Navigate,
+  Route,
+  BrowserRouter as Router,
+  Routes,
+  useLocation,
+} from "react-router-dom";
+import { toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import Cart from "./components/cart/cart";
 import Checkout from "./components/checkout/checkout";
 import Login from "./components/customer/login/login";
@@ -9,20 +18,61 @@ import Add from "./components/food/add-food/add-food";
 import Menu from "./components/food/menu/menu";
 import Navbar from "./components/navbar/navbar";
 
-function App() {
+function PrivateRoute({ userLoggedIn, children }) {
+  return userLoggedIn ? children : <Navigate to="/customer/login" />;
+}
+
+function AppContent({ userLoggedIn }) {
+  const location = useLocation();
+
+  // Paths where the Navbar should be hidden
+  const hideNavbarPaths = ["/customer/login", "/customer/register"];
+
   return (
-    <Router>
-      <Navbar />
+    <>
+      {!hideNavbarPaths.includes(location.pathname) && <Navbar />}
       <Routes>
         <Route path="/customer/register" element={<Register />} />
         <Route path="/customer/login" element={<Login />} />
         <Route path="/customer/profile" element={<ProfilePage />} />
         <Route path="/food/add" element={<Add />} />
         <Route path="/food/menu" element={<Menu />} />
-        <Route path="/" element={<Menu />} />
+        <Route
+          path="/"
+          element={
+            <PrivateRoute userLoggedIn={userLoggedIn}>
+              <Menu />
+            </PrivateRoute>
+          }
+        />
         <Route path="/cart/item" element={<Cart />} />
         <Route path="/checkout" element={<Checkout />} />
       </Routes>
+    </>
+  );
+}
+
+function App() {
+  const [userLoggedIn, setUserLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      try {
+        jwtDecode(token);
+        setUserLoggedIn(true);
+      } catch (error) {
+        console.error("Invalid token:", error);
+        toast.error("Invalid token. Please login again.");
+        localStorage.removeItem("token");
+        setUserLoggedIn(false);
+      }
+    }
+  }, []);
+
+  return (
+    <Router>
+      <AppContent userLoggedIn={userLoggedIn} />
     </Router>
   );
 }
